c77995b4fa2e4ed1d3597c3160dbe208f9fd2033
mugisa <an235582@dal.ca>
2024-01-23 23:30:00 -0400

    implemented loader page

--- script.js
+++ script.js
@@ -41,3 +41,39 @@ window.addEventListener("scroll", function () {
     nav.classList.toggle("sticky", window.scrollY > 0);
     
 })
+
+/** lading screen **/
+
+document.addEventListener("DOMContentLoaded", function () {
+    const counter3  = document.querySelector(".counter-3");
+
+    for (let i = 0; i < 2;i++) {
+        for (let  j = 0; j < 10; j++) {
+            const div = document.createElement("div")
+            div.className = "num";
+            div.textContent = j;
+            counter3.appendChild(div);
+        }
+    }
+
+    const finalDiv = document.createElement("div");
+    finalDiv.className = "num";
+    finalDiv.textContent = "0";
+    counter3.appendChild(finalDiv);
+
+    function animate(counter, duration, delay = 0) {
+        const numHeight = counter.querySelector(".num").
+            clientHeight;
+        const totalDistance =
+            (counter.querySelectorAll(".num").length - 1) *
+            numHeight;
+
+        gasp.to(counter, {
+            y: -totalDistance,
+            duration: duration,
+            delay: delay,
+            ease: "power2.inOut",
+        })
+
+    }
+})
